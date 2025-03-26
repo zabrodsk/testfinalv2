32800f0777402d0608d33b9572fac824d5fa833b
zabrodsk <dusan.zabrodsky@gmail.com>
2025-03-26 11:36:16 +0100

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,35 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`10000c00010102010101020101010101010101010101020101010201010101010101010101010201010201010101010101010101010102010102010101010102010101010202020202020202020202020202020201010201010201010101020101010101010102010102010101010201010101010202020202020202020202020202020200000200000200000002000202020202000002000002000000000202000101000000020000020000000101010100000000000200000200000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath5,sprites.builtin.forestTiles0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
